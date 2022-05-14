cbd244a2e3e58afdf1fd336038d6cdb1aab9371f
lokesh <lokeshns7476@gmail.com>
2022-05-15 03:06:28 +0530

    commiting the staturday changes

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ navlist = document.querySelector('.navlist')
 burger.addEventListener('click', ()=>{
     navbar.classList.toggle('h-nav');
     navlist.classList.toggle('o-nav');
-})
+});
+
+// window.onscroll=()=>{
+//     navbar.classList.remove('h-nav');
+//     navlist.classList.toggle('o-nav');
+// }; 
